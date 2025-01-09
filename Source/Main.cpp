2a226737a35aafd14fc581c26c454704dba75908
Shojine <trintentatar@gmail.com>
2025-01-09 15:53:07 -0700

    added GOL

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,10 +1,10 @@
-#include "CAScene.h"
+#include "GOLScene.h"
 #include <memory>
 #include <SDL.h>
 
 int main(int, char**)
 {
-	std::unique_ptr<CAScene> scene = std::make_unique<CAScene>();
+	std::unique_ptr<GOLScene> scene = std::make_unique<GOLScene>();
 
 	scene->Initialize();
 	while (!scene->IsQuit())
